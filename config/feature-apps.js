210d3ffaa37b0206fa2d8318734f26fda6077300
Dave Laird <kiwiupover@gmail.com>
2017-07-31 11:59:22 -0700

    split two apps

--- config/feature-apps.js
+++ config/feature-apps.js
@@ -1,9 +1,9 @@
 module.exports = {
   'headerapp': [
-    'split-app/helpers/**.*',
-    'split-app/initializers/**.*',
-    'split-app/instance-initializers/**.*',
-    'split-app/services/**.*',
+    // 'split-app/helpers/**.*',
+    // 'split-app/initializers/**.*',
+    // 'split-app/instance-initializers/**.*',
+    // 'split-app/services/**.*',
     'headerapp/templates/application.js',
     'headerapp/app.js',
     'headerapp/router.js',
@@ -15,10 +15,10 @@ module.exports = {
     'vendor/ember-cli/app-boot.js'
   ],
   'lyrvapp': [
-    'split-app/helpers/**.*',
-    'split-app/initializers/**.*',
-    'split-app/instance-initializers/**.*',
-    'split-app/services/**.*',
+    // 'split-app/helpers/**.*',
+    // 'split-app/initializers/**.*',
+    // 'split-app/instance-initializers/**.*',
+    // 'split-app/services/**.*',
     'lyrvapp/templates/application.js',
     'lyrvapp/app.js',
     'lyrvapp/router.js',
@@ -29,32 +29,3 @@ module.exports = {
     'vendor/ember-cli/app-boot.js'
   ]
 }
-
-// module.exports = {
-//   'premium': [
-//     'adapters/offer.js',
-//     'serializers/offer.js',
-//     'serializers/offer-asset.js',
-//     'models/offer.js',
-//     'models/offer-asset.js',
-//     'components/samus/**/*.*',
-//     'components/pr/**/*.*',
-//     'templates/svg/samus/*.*',
-//
-//     'templates/pr/*.*',
-//     'services/claim-offer.js',
-//     'services/premium-geo.js',
-//     'utilities/samus/regions.js',
-//     'utilities/urls/prime-urls.js',
-//
-//     'components/subscribe/**/*.*',
-//     'components/new-feature/baxter/*.*',
-//     'components/front-page/header-bar/premium/**/*.*'
-//   ],
-//   'game-details': [
-//     'components/directory/game-details/**/*.*',
-//     'components/channel-redesign/metadata-box/fl-md/*.*',
-//
-//     'utilities/urls/fuel.js'
-//   ]
-// }
